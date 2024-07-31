d57a14cbb9beb963a190e1fb7d52fa2a636ebeae
RuslanVslv95 <112714732+RuslanSmap@users.noreply.github.com>
2024-07-31 15:00:33 +0300

    styled-works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -10,48 +10,81 @@ import projectImage3 from '../../../assets/images/proj-3.webp'
 import projectImage4 from '../../../assets/images/proj-4.webp'
 import projectImage5 from '../../../assets/images/proj-5.webp'
 import projectImage6 from '../../../assets/images/proj-6.webp'
+import { Container } from '../../../components/Container';
+
 
 export const Works = () => {
     return (
         <StyledWorks>
-            <SectionTitle>Projects</SectionTitle>
-            <SectionText>Things I’ve built so far</SectionText>
-            <FlexWrapper justify={'space-around'} wrap={'wrap'}>
-                <Work title={'Project Tile goes here'}
-                    text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
-                    technology={'Tech stack : HTML , JavaScript, SASS, React'}
-                    src={projectImage1} />
+            <Container>
+                <ProjectsTitle>Projects</ProjectsTitle>
+                <ProjectsText>Things I’ve built so far</ProjectsText>
+                <ProjectsWorks>
+                <FlexWrapper justify={'space-around'} wrap={'wrap'}>
+                    <Work title={'Project Tile goes here'}
+                        text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
+                        // technology={'Tech stack : HTML , JavaScript, SASS, React'}
+                        src={projectImage1} />
 
-                <Work title={'Project Tile goes here'}
-                    text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
-                    technology={'Tech stack : HTML , JavaScript, SASS, React'}
-                    src={projectImage2} />
+                    <Work title={'Project Tile goes here'}
+                        text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
+                        // technology={'Tech stack : HTML , JavaScript, SASS, React'}
+                        src={projectImage2} />
 
-                <Work title={'Project Tile goes here'}
-                    text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
-                    technology={'Tech stack : HTML , JavaScript, SASS, React'}
-                    src={projectImage3} />
+                    <Work title={'Project Tile goes here'}
+                        text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
+                        // technology={'Tech stack : HTML , JavaScript, SASS, React'}
+                        src={projectImage3} />
 
-                <Work title={'Project Tile goes here'}
-                    text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
-                    technology={'Tech stack : HTML , JavaScript, SASS, React'}
-                    src={projectImage4} />
+                    <Work title={'Project Tile goes here'}
+                        text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
+                        // technology={'Tech stack : HTML , JavaScript, SASS, React'}
+                        src={projectImage4} />
 
-                <Work title={'Project Tile goes here'}
-                    text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
-                    technology={'Tech stack : HTML , JavaScript, SASS, React'}
-                    src={projectImage5} />
+                    <Work title={'Project Tile goes here'}
+                        text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
+                        // technology={'Tech stack : HTML , JavaScript, SASS, React'}
+                        src={projectImage5} />
 
-                <Work title={'Project Tile goes here'}
-                    text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
-                    technology={'Tech stack : HTML , JavaScript, SASS, React'}
-                    src={projectImage6} />
-            </FlexWrapper>
+                    <Work title={'Project Tile goes here'}
+                        text={'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'}
+                        // technology={'Tech stack : HTML , JavaScript, SASS, React'}
+                        src={projectImage6} />
+                </FlexWrapper>
+                </ProjectsWorks>
+            </Container>
         </StyledWorks>
     );
 };
 
 const StyledWorks = styled.section`
-    min-height: 100vh;
+    /* min-height: 100vh; */
    
+`
+const ProjectsTitle = styled.h2`
+   margin: 200px 0px 50px 0px;
+   text-align: center;
+   color: #42446E; 
+   font-family: 'Poppins', sans-serif;
+   font-weight: 700px;
+   font-size: 48px;
+   letter-spacing: 0px;
+   line-height: 26px;
+`
+const ProjectsText = styled.p`
+   margin-bottom: 100px;
+   text-align: center;
+   color: #666666; 
+   font-family: 'Poppins', sans-serif;
+   font-weight: 400px;
+   font-size: 32px;
+   letter-spacing: 0px;
+   line-height: 26px;
+`
+
+const ProjectsWorks = styled.div`
+      div:nth-child(3),
+      div:nth-child(6) {
+        margin-right: 0px;
+    }
 `
--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,47 +1,114 @@
 import React from 'react';
 import styled from 'styled-components';
+import iconLink from '../../../../assets/images/link-icon.svg'
+import worksGitLink from '../../../../assets/images/git-link.svg'
 
 type WorkPropsType = {
     title: string
     text: string
-    technology: string
+    // technology: string
     src: string
 }
 
 export const Work = (props: WorkPropsType) => {
     return (
         <StyledWork>
-            <Image src={props.src} alt=''/>
-            <Title>{props.title}</Title>
-            <Text>{props.text}</Text>
-            <Technologies>{props.technology}</Technologies>
-            <Link href={'#'}>Live Preview</Link>
-            <Link href={'#'}>View Code</Link>
+            <Image src={props.src} alt='' />
+            <WorkContainer>
+                <Title>{props.title}</Title>
+                <Text>{props.text}</Text>
+                <Technologies>
+                    <TechnologyTitle>Tech stack : &nbsp;</TechnologyTitle>
+                    <TechnologyText>HTML , JavaScript, SASS, React</TechnologyText>
+                </Technologies>
+                <LinkContainer>
+                    <IconLink src={iconLink}></IconLink>
+                    <Link href={'#'}>Live Preview</Link>
+
+                    <IconLink src={worksGitLink}></IconLink>
+                    <Link href={'#'}>View Code</Link>
+                </LinkContainer>
+            </WorkContainer>
         </StyledWork>
     );
 };
 
+
 const StyledWork = styled.div`
-    background-color: #ffe869;
+    /* background-color: #ffe869; */
     max-width: 375px;
+    max-height: 570px;
     width: 100%;
+    margin-right: 35px;
+    margin-bottom: 60px;
+    border-radius: 20px;
+    box-shadow: 2px 2px 100px 0px rgba(0, 0, 0, 0.2);
+  
 `
 const Image = styled.img`
     width: 100%;
     height: 260px;
     object-fit: cover;
 `
-const Link = styled.a`
+const WorkContainer = styled.div`
+    max-width: 315px;
+    margin: 0px 30px 0px 30px;
+`
 
+const Link = styled.a`
+   
 `
 const Title = styled.h3`
-
+    color: #000000;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 28px;
+    line-height: 26px;
+    letter-spacing: 0px;
+    margin-top: 27px;
+    margin-bottom: 17px;
 `
 
 const Text = styled.p`
-
+    color: #666666;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 26px;
+    letter-spacing: 0px;
+    text-align: left;   
+    margin-bottom: 12px;
 `
 
-const Technologies = styled.span`
+const TechnologyTitle = styled.span`
+    color: #42446E;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 26px;
+    letter-spacing: 0px;
+`
+const TechnologyText = styled.p`
+    color: #42446E;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 26px;
+    letter-spacing: 0px;
+    margin-bottom: 20px;
+`
 
+const Technologies = styled.p`
+    display: flex;
+    justify-content: flex-start;
+`
+
+const LinkContainer = styled.div`
+  a:nth-child(2){
+    margin-right: 48px;
+  }
+  margin-bottom: 25px;
+`
+const IconLink = styled.img`
+    margin-right: 10px;
 `
